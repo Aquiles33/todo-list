b0a86715f93914eb0da1b7f717311c1ce0071513
Aquiles Silva dos Santos <asantos@marlin.net>
2021-07-10 15:07:39 -0300

    :construction: Header: Componente Header finalizado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  public title: string = 'Crie aqui sua lista de tarefas'
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  @Input() title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.typeWrite();
+  }
+
+  typeWrite() {
+    const h1 = document.querySelector<HTMLElement>('.header-title-content');
+    const arrayText = this.title.split('');
+    h1.innerHTML = '';
+    arrayText.forEach((letter, i) => setTimeout(() => h1.innerHTML += letter, 75 * i));
+  }
+}
